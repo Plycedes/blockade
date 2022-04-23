3f16f70e7beb2264b96512bf574cac6b026bd0e7
Abhay Pratap Yadav <abhaypratapyadav340@gmail.com>
2022-04-23 16:58:51 +0530

    First Lecture

---
+++ Blockade/Assets/Scripts/GameManagement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagement : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void GameOver()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+        }        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Blockade/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Hurdle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManagement>().GameOver();
+        }
+    }
+}
